1cdb1d468e44b4e2fb25702c2760fac02db60cd3
Jiří Vrba <jiri@vrba.dev>
2026-02-11 21:45:26 +0100

    Use IPV6

--- config/runtime.exs
+++ config/runtime.exs
@@ -28,15 +28,13 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
-
   config :homecooked, Homecooked.Repo,
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
-    socket_options: [:inet, {:inet_backend, :inet}]
+    socket_options: [:inet6, {:inet_backend, :inet}]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
